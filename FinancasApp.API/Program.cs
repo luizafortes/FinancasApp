5257238d46cba90fd77b2e658622308a9190fc8d
Luiza Fortes <luizafortes@gmail.com>
2025-09-26 08:51:25 -0300

    Criação do Dockerfile e docker-compose

--- FinancasApp.API/Program.cs
+++ FinancasApp.API/Program.cs
@@ -15,10 +15,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
 
 //Swagger
 app.UseSwagger();
