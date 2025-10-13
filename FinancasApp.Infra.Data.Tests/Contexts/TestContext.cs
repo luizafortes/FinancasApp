76d10ce7c813a76839cf2d5f5ba88f4f0cff6a28
Luiza Fortes <luizafortes@gmail.com>
2025-10-13 07:32:06 -0300

    Desenvolvimento dos testes unitários e configuração do build na PIPELINE.

---
+++ FinancasApp.Infra.Data.Tests/Contexts/TestContext.cs
@@ -0,0 +1,26 @@
+﻿using FinancasApp.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancasApp.Infra.Data.Tests.Contexts
+{
+    /// <summary>
+    /// Classe para configurar o DataContext do Entity Framework
+    /// definindo o tipo de conexão para banco de dados de memória
+    /// </summary>
+    public class TestContext
+    {
+        public static DataContext CreateDataContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "FinancasApp")
+                .Options;
+
+            return new DataContext(options);
+        }
+    }
+}
